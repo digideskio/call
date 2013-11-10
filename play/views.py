11e833959c6c00429939a94cf6e66541daa770da
Nic Pottier <nicpottier@gmail.com>
2013-11-10 11:51:21 +0200

    sow

--- play/views.py
+++ play/views.py
@@ -1,15 +1,20 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 from twilio.rest import TwilioRestClient
+from twilio import twiml
+from random import randint
 
 def play(request):
-    return HttpResponse("Hello")
+    r = twiml.Response()
+    offset = randint(1, 9)
+    r.play("http://quiet-atoll-4001.herokuapp.com/static/wavs/sfx_%d.wav" % offset)
+    return HttpResponse(str(r))
 
 def trigger(request):
     if request.method == 'GET' and request.REQUEST.get('pword', 'N') == 'S':
         client = TwilioRestClient()
         call = client.calls.create(to="+250788383383", from_="+12067454100",
-                                   url="http://foo.com/call.xml")
+                                   url="http://quiet-atoll-4001.herokuapp.com/play/")
 
         return HttpResponse("Triggered")
 
